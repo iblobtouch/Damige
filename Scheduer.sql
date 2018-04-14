d4264d146e33c22b76eeeeebb87f732b308bac38
iblobtouch <kingsburynick@ymail.com>
2018-04-14 23:34:28 +0100

    Added a schedule to delete deliveries assigned to expired drivers

--- Scheduer.sql
+++ Scheduer.sql
@@ -9,3 +9,14 @@ ON SCHEDULE
 	UPDATE idCard i1 JOIN driver_state d1 ON i1.State_ID = d1.State_ID
     SET d1.status = "Expired"
     WHERE i1.End_Date < NOW();
+    
+CREATE EVENT `ExpireDelivery` 
+ON SCHEDULE
+         EVERY 1 HOUR
+    ON COMPLETION NOT PRESERVE
+    DO
+    DELETE FROM Delivery dl1 
+    JOIN Driver dr1 ON dl1.Driver_ID = dr1.Driver_ID 
+    JOIN idCard i1 ON dr1.Card_ID = i1.Card_ID 
+    JOIN Driver_State ds1 ON i1.State_ID = ds1.State_ID
+    WHERE ds1.Status = "Expires";
